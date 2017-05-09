69dd6211d9a87a84111730d3e871c671243e15e8
Mayur Joshi <Mayur.Joshi@in.tesco.com>
2017-05-09 22:02:49 +0530

    Bug fix

--- src/scripts/utils/Constants.js
+++ src/scripts/utils/Constants.js
@@ -3,10 +3,10 @@ var baseUrl = 'http://ec2-35-154-248-81.ap-south-1.compute.amazonaws.com';
 // var baseUrl = 'http://localhost:3000';
 var Constants = {
     API: {
-        [ActionTypes.GET_QUESTION_CATEGORIES]: baseUrl + '/categories',
-        [ActionTypes.GET_QUESTIONS]: baseUrl + '/questions',
-        [ActionTypes.REGISTER_USER]: baseUrl + '/signup',
-        [ActionTypes.LOGIN_USER]: baseUrl + '/login'
+        [ActionTypes.GET_QUESTION_CATEGORIES]: (baseUrl + '/categories'),
+        [ActionTypes.GET_QUESTIONS]: baseUrl + ('/questions'),
+        [ActionTypes.REGISTER_USER]: (baseUrl + '/signup'),
+        [ActionTypes.LOGIN_USER]: (baseUrl + '/login')
     }
 };
 
